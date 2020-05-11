1b71dc076fd219859e9f7dc0dc0b18336f2aeb03
hemanth1508 <kvshemanthreddy@gmail.com>
2020-05-11 18:10:49 +0530

    success

---
+++ app/middlewares/uploadProfile.js
@@ -0,0 +1,24 @@
+const aws = require('aws-sdk');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const dotenv = require('dotenv');
+dotenv.config();
+aws.config.update({
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    region: 'ap-south-1'
+});
+
+const s3 = new aws.S3();
+const upload = multer({
+    storage: multerS3({
+        acl: 'public-read',
+        s3,
+        bucket: 'hemanth1508',
+        key: function (req, file, cb) {
+            req.file = file.originalname;
+            cb(null, file.originalname);
+        }
+    })
+});
+module.exports = upload;
